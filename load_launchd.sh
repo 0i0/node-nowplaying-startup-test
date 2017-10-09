94d6b018dede83952c2fe7988e3524ee65656675
Lior hakim <leo@Liors-MacBook-Pro.local>
2017-10-09 09:43:43 +0300

    run deamon as user

--- load_launchd.sh
+++ load_launchd.sh
@@ -2,7 +2,7 @@ while true; do
     read -p "would you like me to start node-nowplaying-startup-test server on startup?" yn
     case $yn in
         [Yy]* ) echo "setting up launcDeamond...";
-				cat com.liorhakim.node-nowplaying-startup-test-template.plist | awk -v P="$(pwd)" '{gsub(/pwd/,P,$0); print}' > com.liorhakim.node-nowplaying-startup-test.plist
+				cat com.liorhakim.node-nowplaying-startup-test-template.plist | awk -v P="$(pwd)" -v U="$USER" '{gsub(/pwd/,P,$0);{gsub(/user/,U,$0); print}' > com.liorhakim.node-nowplaying-startup-test.plist
 				sudo cp com.liorhakim.node-nowplaying-startup-test.plist /Library/LaunchDaemons/com.liorhakim.node-nowplaying-startup-test.plist
 				sudo chown root:wheel /Library/LaunchDaemons/com.liorhakim.node-nowplaying-startup-test.plist
 				sudo launchctl load  /Library/LaunchDaemons/com.liorhakim.node-nowplaying-startup-test.plist
